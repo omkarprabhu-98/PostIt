56853aac16a2dab7e36736274e7e3424f8dfa0fd
Omkar Prabhu <omkkarprabuinfinity@gmail.com>
2017-06-14 18:35:04 +0530

    sign in and login tab completed

---
+++ angular/controllers/signUpController.js
@@ -0,0 +1,29 @@
+/**
+ * Created by Admin on 14-Jun-17.
+ */
+
+
+
+angular.module('SignIn').controller("signUpController",['$scope', function($scope){
+
+
+
+            this.submit = function(){
+
+
+
+
+
+
+
+
+
+                // window.location =  "test.html";
+            };
+
+
+
+    }
+]
+
+);
